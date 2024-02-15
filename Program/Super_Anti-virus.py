4ddb11561806c79ba863c06b20c72a2ddf4ad6d1
sun589 <73369511+sun589@users.noreply.github.com>
2024-02-15 20:18:44 +0800

    Create Super_Anti-virus.py

---
+++ Program/Super_Anti-virus.py
@@ -0,0 +1,21 @@
+print("欢迎使用Super Anti-virus!")
+if time.time() % 35 == 0:
+    print("更新最新病毒库...")
+    time.sleep(4)
+if input("是否进行扫描?(y/n):") == 'y':
+    for x in range(1000000, 1001230):
+        print(f"\r正在扫描0x{str(x).rjust(7, '0')}",end='',flush=True)
+        time.sleep(0.00001)
+        x += 3
+    print()
+    if n <= 10:
+        if input("检测到编号Ruvis-WaHjkP病毒,是否立即清除?(y/n):") == 'y':
+            print("修复中...")
+            time.sleep(5)
+            n = 77
+            with open(r"c:\superdos_boot.supersystem",'w') as f:
+                f.write('')
+            print("已修复并安装防攻击补丁,请重启应用更新!")
+    else:
+        print("未发现任何病毒!")
+    input()
